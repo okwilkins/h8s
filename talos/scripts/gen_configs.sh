86f2823d66329c6f999a0d140d4c8511aa5ae4c5
Oliver Wilkins <okwilkins@googlemail.com>
2025-03-22 02:36:34 +0000

    feat(talos): Generate Cilium manifests for machine config

--- talos/scripts/gen_configs.sh
+++ talos/scripts/gen_configs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Generates the configs for Talos, controlplanes and workers
 
+set -e
+
 for var in XDG_CONFIG_HOME NODE_1_IP NODE_2_IP; do
     eval "value=\$$var"
     if [ -z "$value" ]; then
@@ -17,6 +19,24 @@ if [ ! -f $TALOS_SECRET_FILE ]; then
 else
     echo "Talos secrets already exists (${TALOS_SECRET_FILE}), skipping..."
 fi
+
+
+# Generate the Cilium manifests here so that when the cluster starts it can network via Cilium's CNI
+# Also gen here so that sensitive keys are not committed  to a repo
+CILIUM_VER="1.16.3"
+echo "Generating Cilium version $CILIUM_VER manifests..."
+helm repo add cilium https://helm.cilium.io/
+export CILIUM_MANIFEST=$(
+    helm template cilium cilium/cilium \
+        --version $CILIUM_VER \
+        --namespace kube-system \
+        -f ../cilium/environments/prod/values.yaml
+)
+
+TMP_FILE=$(mktemp /tmp/cilium_patch.XXXXXX)
+yq --null-input '.cluster.inlineManifests = [{"name": "cilium", "contents": env(CILIUM_MANIFEST)}]' | \
+    # For some reason YQ's literal style doesn't work so using sed to put in the pipe char...?
+    sed 's/contents:\([[:space:]]*\)/contents: |/' > $TMP_FILE
 
 echo "Generating talosconfig..."
 talosctl gen config \
@@ -36,6 +56,7 @@ talosctl gen config \
     --config-patch @machine_patches/controlplane_worker_1.yaml \
     --config-patch @machine_patches/machine_patch.yaml \
     --config-patch @cluster_patch.yaml \
+    --config-patch @$TMP_FILE \
     talos-homelab \
     https://$NODE_1_IP:6443
 
@@ -48,9 +69,11 @@ talosctl gen config \
     --config-patch @machine_patches/controlplane_worker_2.yaml \
     --config-patch @machine_patches/machine_patch.yaml \
     --config-patch @cluster_patch.yaml \
+    --config-patch @$TMP_FILE \
     talos-homelab \
     https://$NODE_2_IP:6443
 
+rm $TMP_FILE
 
 talosctl config endpoint \
     $NODE_1_IP $NODE_2_IP \
