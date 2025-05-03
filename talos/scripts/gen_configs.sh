1856afa989aa04e41d16d17153e8a36992b6b299
Oliver Wilkins <okwilkins@googlemail.com>
2025-05-03 17:56:28 +0100

    refactor(talos): Remove Cilium from gen script

--- talos/scripts/gen_configs.sh
+++ talos/scripts/gen_configs.sh
@@ -20,28 +20,6 @@ else
     echo "Talos secrets already exists (${TALOS_SECRET_FILE}), skipping..."
 fi
 
-
-# Generate the Cilium manifests here so that when the cluster starts it can network via Cilium's CNI
-# Also gen here so that sensitive keys are not committed  to a repo
-CILIUM_VER="1.17.2"
-echo "Generating Cilium version $CILIUM_VER manifests..."
-
-TMP_CILIUM_FILE=$(mktemp /tmp/cilium_patch.XXXXXX)
-helm repo add cilium https://helm.cilium.io/
-helm template cilium cilium/cilium \
-    --version $CILIUM_VER \
-    --namespace kube-system \
-    -f ../cilium/environments/prod/values.yaml > $TMP_CILIUM_FILE
-
-TMP_FILE=$(mktemp /tmp/cilium_patch.XXXXXX)
-cat > $TMP_FILE <<EOF
-cluster:
-  inlineManifests:
-    - name: cilium
-      contents: |
-$(sed 's/^/        /' $TMP_CILIUM_FILE)
-EOF
-
 echo "Generating talosconfig..."
 talosctl gen config \
     --with-secrets $TALOS_SECRET_FILE \
@@ -60,7 +38,6 @@ talosctl gen config \
     --config-patch @machine_patches/controlplane_worker_1.yaml \
     --config-patch @machine_patches/machine_patch.yaml \
     --config-patch @cluster_patch.yaml \
-    --config-patch @$TMP_FILE \
     talos-homelab \
     https://$NODE_1_IP:6443
 
@@ -73,12 +50,8 @@ talosctl gen config \
     --config-patch @machine_patches/controlplane_worker_2.yaml \
     --config-patch @machine_patches/machine_patch.yaml \
     --config-patch @cluster_patch.yaml \
-    --config-patch @$TMP_FILE \
     talos-homelab \
     https://$NODE_2_IP:6443
-
-rm $TMP_CILIUM_FILE
-rm $TMP_FILE
 
 talosctl config endpoint \
     $NODE_1_IP $NODE_2_IP \
