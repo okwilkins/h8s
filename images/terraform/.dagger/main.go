13140d64f8059cbb1f250c7957c88d97367815b1
Oliver Wilkins <okwilkins@googlemail.com>
2025-06-08 14:29:37 +0100

    feat(images/terraform): Init image and Dagger

---
+++ images/terraform/.dagger/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"dagger/terraform-image/internal/dagger"
+	"fmt"
+	"strings"
+)
+
+type TerraformImage struct{}
+
+// Build and push image from existing Dockerfile
+func (m *TerraformImage) Build(ctx context.Context, src *dagger.Directory) (string, error) {
+	var platforms = []dagger.Platform{
+		"linux/amd64",
+		"linux/arm64",
+	}
+	platformVariants := make([]*dagger.Container, 0, len(platforms))
+
+	for _, platform := range platforms {
+		platformParts := strings.Split(string(platform), "/")
+		if len(platformParts) != 2 {
+			return "", fmt.Errorf("invalid platform format: %s", platform)
+		}
+		platformOs := platformParts[0]
+		platformArch := platformParts[1]
+
+		buildArgs := []dagger.BuildArg{
+			{
+				Name:  "GOARCH",
+				Value: platformArch,
+			},
+			{
+				Name:  "GOOS",
+				Value: platformOs,
+			},
+		}
+		buildOpts := dagger.DirectoryDockerBuildOpts{Platform: platform, BuildArgs: buildArgs}
+		ctr := src.DockerBuild(buildOpts)
+		platformVariants = append(platformVariants, ctr)
+	}
+
+	imageDigest, err := dag.Container().
+		Publish(ctx, "ttl.sh/coredns:1h", dagger.ContainerPublishOpts{
+			PlatformVariants: platformVariants,
+		})
+
+	if err != nil {
+		return "", err
+	}
+
+	return imageDigest, nil
+}
---
+++ images/terraform/README.md
@@ -0,0 +1,11 @@
+# Image: Terraform
+
+This Dockerfile builds a non-root version of Terraform.
+
+## Available Build Arguments
+
+|       Argument      | Default Value |                   Description                   |
+|:-------------------:|:-------------:|:-----------------------------------------------:|
+| `TERRAFORM_VERION`  |               |       The version of the image to be used for Terraform       |
+
+
