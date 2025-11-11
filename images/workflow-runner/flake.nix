3f0c2ede3ec93393bd30cfb9fe91aa38981d924a
Oliver Wilkins <okwilkins@googlemail.com>
2025-11-11 20:54:43 +0000

    feat(images/workflow-runner): Nix flake init

---
+++ images/workflow-runner/flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Flake for workflow runner image creation";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.go
+            pkgs.cosign
+            pkgs.jq
+            pkgs.go-task
+            pkgs.buildkit
+          ];
+        };
+      }
+    );
+}
