a2456e35823f7b7cfcdc25f59e3197fd4c89c6f1
Oliver Wilkins <okwilkins@googlemail.com>
2026-03-06 21:01:32 +0000

    refactor(flake): Make INFRA_ROOT and PROJECT_ROOT

--- flake.nix
+++ flake.nix
@@ -35,9 +35,11 @@
         devShells.default = pkgs.mkShell {
           buildInputs = tools;
           shellHook = ''
-            export TF_VAR_project_root="$(git rev-parse --show-toplevel 2>/dev/null)"
+            export PROJECT_ROOT="$(git rev-parse --show-toplevel 2>/dev/null)"
+            export TF_VAR_project_root=$PROJECT_ROOT
             # TODO: Change this to just be infrastructure when refactoring everything together: i.e. move terraform into this project
-            export TF_VAR_infra_root=$TF_VAR_project_root/infrastructure/bootstrap
+            export INFRA_ROOT=$PROJECT_ROOT/infrastructure/bootstrap
+            export TF_VAR_infra_root=$INFRA_ROOT
           '';
         };
 
