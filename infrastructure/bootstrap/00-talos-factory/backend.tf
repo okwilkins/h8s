a81fdc4c67af67c65b3b940e13a832d6f41aa5e9
Oliver Wilkins <okwilkins@googlemail.com>
2026-03-06 19:15:03 +0000

    fix(bootstrap): Use Tofu static variables

--- infrastructure/bootstrap/00-talos-factory/backend.tf
+++ infrastructure/bootstrap/00-talos-factory/backend.tf
@@ -1,5 +1,5 @@
 terraform {
   backend "local" {
-    path = "$INFRA_ROOT/states/00-talos-factory.tfstate"
+    path = "${var.infra_root}/states/00-talos-factory.tfstate"
   }
 }
---
+++ infrastructure/bootstrap/shared/variables.tf
@@ -0,0 +1,9 @@
+# ============================================================
+# General
+# ============================================================
+
+variable "infra_root" {
+  description = "Root directory for infrastructure"
+  type        = string
+}
+
