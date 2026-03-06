5666b06409a91a25834175eee8d6997ea43acc7f
Oliver Wilkins <okwilkins@googlemail.com>
2026-03-06 19:37:06 +0000

    feat(bootstrap/providers): Variables to proxmox

--- infrastructure/bootstrap/shared/providers.tf
+++ infrastructure/bootstrap/shared/providers.tf
@@ -64,3 +64,10 @@ terraform {
     }
   }
 }
+
+provider "proxmox" {
+  endpoint = var.proxmox_endpoint
+  username = var.proxmox_username
+  password = var.proxmox_password
+  insecure = var.proxmox_insecure
+}
