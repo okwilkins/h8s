4d67494a037917a99b3672395fcb43187f1aaf4c
Oliver Wilkins <okwilkins@googlemail.com>
2026-03-15 12:22:36 +0000

    feat(infra/vault-resources): Authelia encryption key

--- infrastructure/07-vault-resources-provision/main.tf
+++ infrastructure/07-vault-resources-provision/main.tf
@@ -78,6 +78,11 @@ resource "random_password" "cnpg_terraform_backend_password" {
   override_special = "!#%&*()-_=+[]{}<>?"
 }
 
+resource "random_password" "authelia_encryption_key" {
+  length  = 64
+  special = false
+}
+
 resource "random_password" "pocket_id_encryption_key" {
   length  = 32
   special = false
@@ -364,6 +369,32 @@ resource "null_resource" "vault_secret_cnpg_authelia" {
         vault kv put kubernetes-homelab/cnpg/cnpg-authelia-prod-app-user-credentials \\
           username=authelia \\
           password='${random_password.cnpg_authelia_password.result}' || exit 1
+      "
+    EOT
+  }
+
+  depends_on = [data.terraform_remote_state.vault_init]
+}
+
+resource "null_resource" "vault_secret_authelia_encryption" {
+  triggers = {
+    secret_hash = md5("encryption-key=${random_password.authelia_encryption_key.result}")
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      source ${var.infra_root}/scripts/common.sh
+      load_tf_kube_env
+      create_cert_dir
+
+      VAULT_TOKEN=$(jq -r '.root_token' ${data.terraform_remote_state.vault_init.outputs.vault_init_file})
+      export VAULT_TOKEN
+
+      kubectl_wrapper exec vault-0 -n vault -- /bin/sh -c "
+        export VAULT_TOKEN=\"$VAULT_TOKEN\"
+        vault login -no-store \"\$VAULT_TOKEN\" || exit 1
+        vault kv put kubernetes-homelab/authelia/encryption-key \\
+          encryption-key='${random_password.authelia_encryption_key.result}' || exit 1
       "
     EOT
   }
